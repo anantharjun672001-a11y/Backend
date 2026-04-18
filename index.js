2dd2b347610caa0bcedb52035339345f1d703627
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-04-18 14:40:14 +0530

    cors fixed

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
-app.use(cors(
-  {
-    origin: ["*","https://ai-code-reviewer-blue-six.vercel.app/"],
-  }
-));
+
+app.use(cors({
+  origin: "https://ai-code-reviewer-blue-six.vercel.app"
+}));
 app.use(express.json());
 
 //DB Connect
